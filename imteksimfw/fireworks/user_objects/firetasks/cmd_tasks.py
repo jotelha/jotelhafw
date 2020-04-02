1d9304db9e776234af82bfe6fb2c0a59b6313c4f
Johannes Hörmann <johannes.hoermann@imtek.uni-freiburg.de>
2020-04-02 13:36:38 +0200

    TYP

--- imteksimfw/fireworks/user_objects/firetasks/cmd_tasks.py
+++ imteksimfw/fireworks/user_objects/firetasks/cmd_tasks.py
@@ -126,7 +126,7 @@ def get_nested_dict_value(d, key):
     except KeyError:
         key = key.replace("->", ".")  # make sure no -> left
         try:
-            key_prefix, key_suffix = key.split('.')
+            key_prefix, key_suffix = key.split('.',1)
         except ValueError:   # not enough values to unpack
             raise KeyError
 
