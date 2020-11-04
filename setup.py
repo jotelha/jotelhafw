b4e144445a6dc87873c8b942b8aa3cb76835a178
Johannes Laurin Hörmann <johannes.hoermann@imtek.uni-freiburg.de>
2020-11-04 15:34:02 +0100

    MAINT: Typo, test matrix

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
                 # ssh tests
                 'mock-ssh-server>=0.8.1',
                 # dtool smb tasks tests
-                'reuests>=2.24.0',
+                'requests>=2.24.0',
                 'urllib3>=1.25.11',
             ],
         },
